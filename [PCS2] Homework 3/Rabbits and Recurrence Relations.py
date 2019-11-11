00bdaeb98395c3c815a02a40141fa7a2c644168e
ceccacci1841630 <57144570+ceccacci1841630@users.noreply.github.com>
2019-11-11 11:25:03 +0100

    Delete Rabbits and Recurrence Relations.py

--- [PCS2] Homework 3/Rabbits and Recurrence Relations.py
+++
@@ -1,8 +0,0 @@
-def rabbit_fib(n,k):
-    rabbits_alive = [1, 1+k]
-    for _ in range(n-2):
-        offspring = k * rabbits_alive[-2]
-        rabbits_alive.append(offspring + rabbits_alive [-1])
-    return rabbits_alive
-    
-print(rabbit_fib(33, 5)[-2])
